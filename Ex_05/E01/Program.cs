c2e030d76f50ad5478d3092b181f52f57f0fd675
[1] <[1.asus.rfrf]>
2022-10-24 19:39:45 +0300

    Add folder Ex_5

---
+++ Ex_05/E01/Program.cs
@@ -0,0 +1,11 @@
+﻿// E01 Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+double k1 = 2;
+int b1 = -1;
+double k2 = -3;
+int b2 = 1;
+
+double x = Math.Round((b1 - b2) / (k2 - k1), 2);
+double y = Math.Round((k2 * x + b2), 2);
+
+Console.WriteLine($"Координаты точки пересечения двух прямых: ({x}; {y})");
---
+++ Ex_05/E03/Program.cs
@@ -0,0 +1,37 @@
+﻿// E03 Написать программу копирования массива
+
+
+// Задаем исходные параметры для создания рандомного массива
+
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+//Console.WriteLine();
+Console.Write("Задайте нижнюю границу рандома: ");
+int min = int.Parse(Console.ReadLine() ?? "0");
+//Console.WriteLine();
+Console.Write("Задайте верхнюю границу рандома: ");
+int max = int.Parse(Console.ReadLine() ?? "0");
+
+
+// Создаем рандомный массив и выводим на экран
+
+int[] array = new int[n];
+Random rand = new Random();
+Console.Write("Первоначальный массив: ");
+for (int i = 0; i < n; i++)
+{
+    array[i] = rand.Next(min, max + 1);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+
+// Создаем копию массива и выводим на экран
+
+int[] arrayCopy = new int[array.Length];
+Console.Write("Копия нашего массива:  ");
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy[i] = array[i];
+    Console.Write($"{arrayCopy[i]} ");
+}
